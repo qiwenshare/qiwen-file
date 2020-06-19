89cd00707d81319b4bbea69748de9cb2c6800acc
WeiHongBin <1602586227@qq.com>
2020-06-19 10:59:06 +0800

    漏提交了 FileMapper.java

--- file-web/src/main/java/com/mac/scp/mapper/FileMapper.java
+++ file-web/src/main/java/com/mac/scp/mapper/FileMapper.java
@@ -1,9 +1,14 @@
 package com.mac.scp.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.mac.scp.domain.FileBean;
+import com.mac.scp.entity.File;
+import org.apache.ibatis.annotations.Mapper;
 
-public interface FileMapper extends BaseMapper<FileBean> {
-
-
+/**
+ * 文件 Mapper 接口
+ *
+ * @author WeiHongBin
+ */
+@Mapper
+public interface FileMapper extends BaseMapper<File> {
 }
