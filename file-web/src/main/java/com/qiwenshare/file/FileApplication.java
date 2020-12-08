6f002496f2a7db52396b3790250445c37e0cf0d1
马超 <machaop@dcits.com>
2020-12-08 13:12:24 +0800

    部署war

--- file-web/src/main/java/com/qiwenshare/file/FileApplication.java
+++ file-web/src/main/java/com/qiwenshare/file/FileApplication.java
@@ -3,6 +3,8 @@ package com.qiwenshare.file;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -13,10 +15,16 @@ import org.springframework.web.client.RestTemplate;
 @EnableFeignClients
 @MapperScan("com.qiwenshare.file.mapper")
 @EnableDiscoveryClient
-public class FileApplication {
+public class FileApplication  extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(FileApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(FileApplication.class);
     }
 
 
