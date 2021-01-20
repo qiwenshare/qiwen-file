5253791bedd27cd2605f510b034f0084f3a0d871
马超 <machaop@dcits.com>
2021-01-20 17:40:41 +0800

    新增回收站文件还原接口

--- file-web/src/main/java/com/qiwenshare/file/controller/RecoveryFileController.java
+++ file-web/src/main/java/com/qiwenshare/file/controller/RecoveryFileController.java
@@ -1,14 +1,20 @@
 package com.qiwenshare.file.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.qiwenshare.common.cbb.DateUtil;
 import com.qiwenshare.common.cbb.RestResult;
+import com.qiwenshare.common.util.PathUtil;
 import com.qiwenshare.file.api.IRecoveryFileService;
 import com.qiwenshare.file.api.IUserFileService;
+import com.qiwenshare.file.api.IUserService;
 import com.qiwenshare.file.domain.RecoveryFile;
+import com.qiwenshare.file.domain.UserBean;
 import com.qiwenshare.file.domain.UserFile;
 import com.qiwenshare.file.dto.BatchDeleteRecoveryFileDTO;
 import com.qiwenshare.file.dto.DeleteRecoveryFileDTO;
-import com.qiwenshare.file.service.RecoveryFileService;
+import com.qiwenshare.file.dto.recoveryfile.RestoreFileDto;
 import com.qiwenshare.file.vo.file.RecoveryFileListVo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -27,6 +33,8 @@ public class RecoveryFileController {
     IRecoveryFileService recoveryFileService;
     @Resource
     IUserFileService userFileService;
+    @Resource
+    IUserService userService;
 
 
     @Operation(summary = "删除回收文件", description = "删除回收文件", tags = {"recoveryfile"})
@@ -76,4 +84,62 @@ public class RecoveryFileController {
         return restResult;
     }
 
+    @Operation(summary = "还原文件", description = "还原文件", tags = {"recoveryfile"})
+    @RequestMapping(value = "/restorefile", method = RequestMethod.POST)
+    @ResponseBody
+    public RestResult restoreFile(RestoreFileDto restoreFileDto, @RequestHeader("token") String token) {
+        UserBean sessionUserBean = userService.getUserBeanByToken(token);
+        LambdaUpdateWrapper<UserFile> userFileLambdaUpdateWrapper = new LambdaUpdateWrapper<>();
+        userFileLambdaUpdateWrapper.set(UserFile::getDeleteFlag, 0)
+                .set(UserFile::getDeleteBatchNum, "")
+                .eq(UserFile::getDeleteBatchNum, restoreFileDto.getDeleteBatchNum());
+        userFileService.update(userFileLambdaUpdateWrapper);
+
+        String filePath = PathUtil.getParentPath(restoreFileDto.getFilePath());
+        while(filePath.indexOf("/") != -1) {
+            String fileName = filePath.substring(filePath.lastIndexOf("/") + 1);
+            filePath = PathUtil.getParentPath(filePath);
+            LambdaQueryWrapper<UserFile> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+            lambdaQueryWrapper.eq(UserFile::getFilePath, filePath)
+                    .eq(UserFile::getDeleteFlag, 0)
+                    .eq(UserFile::getUserId, sessionUserBean.getUserId());
+            List<UserFile> userFileList = userFileService.list(lambdaQueryWrapper);
+            if (userFileList.size() == 0) {
+                UserFile userFile = new UserFile();
+                userFile.setUserId(sessionUserBean.getUserId());
+                userFile.setFileName(fileName);
+                userFile.setFilePath(filePath);
+                userFile.setDeleteFlag(0);
+                userFile.setIsDir(1);
+                userFile.setUploadTime(DateUtil.getCurrentTime());
+
+                userFileService.save(userFile);
+            }
+//            else {
+//
+//                LambdaUpdateWrapper<UserFile> updateUserfileWp = new LambdaUpdateWrapper<>();
+//                updateUserfileWp.set(UserFile::getDeleteBatchNum, "")
+//                        .set(UserFile::getDeleteFlag, "0")
+//                        .eq(UserFile::getFilePath, filePath).eq(UserFile::getUserId, sessionUserBean.getUserId());
+//                userFileService.update(updateUserfileWp);
+//            }
+        }
+
+        LambdaQueryWrapper<RecoveryFile> recoveryFileServiceLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        recoveryFileServiceLambdaQueryWrapper.eq(RecoveryFile::getDeleteBatchNum, restoreFileDto.getDeleteBatchNum());
+        recoveryFileService.remove(recoveryFileServiceLambdaQueryWrapper);
+
+
+        return RestResult.success().message("还原成功！");
+    }
+
 }
+
+
+
+
+
+
+
+
+
---
+++ file-web/src/main/java/com/qiwenshare/file/dto/recoveryfile/RestoreFileDto.java
@@ -0,0 +1,9 @@
+package com.qiwenshare.file.dto.recoveryfile;
+
+import lombok.Data;
+
+@Data
+public class RestoreFileDto {
+    private String deleteBatchNum;
+    private String filePath;
+}
