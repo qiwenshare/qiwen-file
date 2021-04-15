ca548c7d1256f290cb8fb3c8611dea5333c54468
马超 <1162714483@qq.com>
2021-04-16 00:02:51 +0800

    删除无用代码

--- file-common/src/main/java/com/qiwenshare/common/operation/download/product/FastDFSDownloader.java
+++ file-common/src/main/java/com/qiwenshare/common/operation/download/product/FastDFSDownloader.java
@@ -4,7 +4,6 @@ import com.github.tobato.fastdfs.proto.storage.DownloadByteArray;
 import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.qiwenshare.common.operation.download.domain.DownloadFile;
 import com.qiwenshare.common.operation.download.Downloader;
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
