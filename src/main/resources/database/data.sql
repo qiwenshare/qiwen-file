aebcd71254a31a2e704c08dc184e4836c2c267ef
马超 <1162714483@qq.com>
2021-08-01 14:30:18 +0800

    新建文档和解压适配优化

--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -1,4 +1,3 @@
--- 初始化用户
 insert ignore into role (roleId, role, description) values (1, 'admin', '管理员'),(2, 'user', '普通用户');
 insert ignore into permission (permissionId, permission) values (1, 'admin'),(2, 'user');
 
