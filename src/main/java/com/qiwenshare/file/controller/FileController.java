f5a9e874aaa2cc49de9b8c2cd6a91c2ecc31145f
马超 <machaop@dcits.com>
2021-08-05 12:55:10 +0800

    修复OSS重命名失败问题

--- src/main/java/com/qiwenshare/file/controller/FileController.java
+++ src/main/java/com/qiwenshare/file/controller/FileController.java
@@ -171,33 +171,13 @@ public class FileController {
                     userFile.getFilePath() + userFile.getFileName() + "/", sessionUserBean.getUserId());
         } else {
             FileBean file = fileService.getById(userFile.getFileId());
-            if (file.getStorageType() == 1) {
 
-                String fileUrl = file.getFileUrl();
-                String newFileUrl = fileUrl.replace(userFile.getFileName(), renameFileDto.getFileName());
-                RenameFile renameFile = new RenameFile();
-                renameFile.setSrcName(fileUrl.substring(1));
-                renameFile.setDestName(newFileUrl.substring(1));
-                ufopFactory.getRenamer(file.getStorageType()).rename(renameFile);
-                LambdaUpdateWrapper<FileBean> lambdaUpdateWrapper = new LambdaUpdateWrapper<>();
-                lambdaUpdateWrapper
-                        .set(FileBean::getFileUrl, newFileUrl)
-                        .eq(FileBean::getFileId, file.getFileId());
-                fileService.update(lambdaUpdateWrapper);
+            LambdaUpdateWrapper<UserFile> lambdaUpdateWrapper = new LambdaUpdateWrapper<>();
+            lambdaUpdateWrapper.set(UserFile::getFileName, renameFileDto.getFileName())
+                    .set(UserFile::getUploadTime, DateUtil.getCurrentTime())
+                    .eq(UserFile::getUserFileId, renameFileDto.getUserFileId());
+            userFileService.update(lambdaUpdateWrapper);
 
-                LambdaUpdateWrapper<UserFile> userFileLambdaUpdateWrapper = new LambdaUpdateWrapper<>();
-                userFileLambdaUpdateWrapper
-                        .set(UserFile::getFileName, renameFileDto.getFileName())
-                        .set(UserFile::getUploadTime, DateUtil.getCurrentTime())
-                        .eq(UserFile::getUserFileId, renameFileDto.getUserFileId());
-                userFileService.update(userFileLambdaUpdateWrapper);
-            } else {
-                LambdaUpdateWrapper<UserFile> lambdaUpdateWrapper = new LambdaUpdateWrapper<>();
-                lambdaUpdateWrapper.set(UserFile::getFileName, renameFileDto.getFileName())
-                        .set(UserFile::getUploadTime, DateUtil.getCurrentTime())
-                        .eq(UserFile::getUserFileId, renameFileDto.getUserFileId());
-                userFileService.update(lambdaUpdateWrapper);
-            }
 
         }
         fileDealComp.uploadESByUserFileId(renameFileDto.getUserFileId());
