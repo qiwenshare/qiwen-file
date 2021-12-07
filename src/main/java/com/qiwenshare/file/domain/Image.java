c84d4bf9c623d11afff2bfaf1b940108a45fb83e
MAC <1162714483@qq.com>
2021-12-07 23:41:49 +0800

    fix(图片预览): 图片预览优化

---
+++ src/main/java/com/qiwenshare/file/domain/Image.java
@@ -0,0 +1,32 @@
+package com.qiwenshare.file.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * @author MAC
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/12/7 22:05
+ */
+@Data
+@Table(name = "image")
+@Entity
+@TableName("image")
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @TableId(type = IdType.AUTO)
+    @Column(columnDefinition="bigint(20)")
+    private Long imageId;
+    @Column(columnDefinition = "bigint(20)")
+    private Long fileId;
+    @Column(columnDefinition="int(5)")
+    private Integer imageWidth;
+    @Column(columnDefinition="int(5)")
+    private Integer imageHeight;
+}
---
+++ src/main/java/com/qiwenshare/file/mapper/ImageMapper.java
@@ -0,0 +1,12 @@
+package com.qiwenshare.file.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qiwenshare.file.domain.Image;
+
+public interface ImageMapper extends BaseMapper<Image> {
+
+
+
+
+}
