d36a28c23795ed1d6b16883f8defa61b9a63777d
MAC <1162714483@qq.com>
2021-12-28 22:38:20 +0800

    fix(分页): 提交分页配置文件

---
+++ src/main/java/com/qiwenshare/file/config/mybitisplus/MybatisPlusConfig.java
@@ -0,0 +1,21 @@
+package com.qiwenshare.file.config.mybitisplus;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//Spring boot方式
+@Configuration
+public class MybatisPlusConfig {
+    // 最新版
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
+
