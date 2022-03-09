d6c84019f66e3be8be676acdc6e7ab9edf8751b2
MAC <1162714483@qq.com>
2022-03-09 21:00:45 +0800

    fix(系统参数): 获取系统参数组接口优化

--- src/main/java/com/qiwenshare/file/controller/SysParamController.java
+++ src/main/java/com/qiwenshare/file/controller/SysParamController.java
@@ -15,7 +15,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Tag(name = "系统参数管理")
 @RestController
@@ -27,13 +29,18 @@ public class SysParamController {
     @Operation(summary = "查询系统参数组", tags = {"系统参数管理"})
     @RequestMapping(value = "/grouplist", method = RequestMethod.GET)
     @ResponseBody
-    public RestResult<SysParam> groupList(
+    public RestResult<Map> groupList(
             @Parameter(description = "查询参数dto", required = false)
                     QueryGroupParamDTO queryGroupParamDTO
     ) {
         List<SysParam> list = sysParamService.list(new QueryWrapper<SysParam>().lambda().eq(SysParam::getGroupName, queryGroupParamDTO.getGroupName()));
+        Map<String, Object> result = new HashMap<>();
 
-        return RestResult.success().data(list);
+        for (SysParam sysParam : list) {
+            result.put(sysParam.getSysParamKey(), sysParam.getSysParamValue());
+        }
+
+        return RestResult.success().data(result);
     }
 
 
