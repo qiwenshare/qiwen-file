e1c10d85b369fcd1321f3416982e5cc5e3f14c40
MAC <1162714483@qq.com>
2023-05-01 22:28:04 +0800

    文件删除和分享文件功能报错修复

--- src/main/java/com/qiwenshare/file/dto/sharefile/ShareFileDTO.java
+++ src/main/java/com/qiwenshare/file/dto/sharefile/ShareFileDTO.java
@@ -3,11 +3,14 @@ package com.qiwenshare.file.dto.sharefile;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @Schema(name = "分享文件DTO",required = true)
 public class ShareFileDTO {
-    @Schema(description="文件集合")
-    private String files;
+    @Schema(description="文件Id集合", required = true)
+    @NotEmpty
+    private String userFileIds;
     @Schema(description = "过期日期", example="2020-05-23 22:10:33")
     private String endTime;
     @Schema(description = "分享类型", example="0公共分享，1私密分享，2好友分享")
